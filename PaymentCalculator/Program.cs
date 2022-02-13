342ba0f55ee969fa6661ca83f10d597baf75151e
Robert Macias <rsmacias@gmail.com>
2022-02-12 23:46:46 -0500

    Se remueve código comentado para continuar mejorando el código.

--- PaymentCalculator/Program.cs
+++ PaymentCalculator/Program.cs
@@ -11,35 +11,6 @@ namespace PaymentCalculator {
         public static IConfigurationRoot _config;
         CultureInfo provider = CultureInfo.InvariantCulture;
 
-        /*static void Main(string[] args) {
-            double moneyToPay = 0;
-
-            var rangeIni = new DateTime(2022, 02, 12, 9, 0, 0); 
-            var rangeFin = new DateTime(2022, 02, 12, 18, 0, 0); 
-
-
-            var ini = new DateTime(2022, 02, 12, 8, 0, 0); //8   15    10
-            var fin = new DateTime(2022, 02, 12, 12, 0, 0); // 13   19    12
-
-            var hours = GetWorkingHours(rangeIni, rangeFin, ini, fin);
-            Console.WriteLine($"Config   >>>> {rangeIni.ToShortTimeString()} - {rangeFin.ToShortTimeString()}");
-            Console.WriteLine($"Workging >>>> {ini.ToShortTimeString()} - {fin.ToShortTimeString()}");
-            Console.WriteLine($"Hours    >>>> {hours}");
-
-            //var result = DateTime.Compare(ini, rangeIni);
-            //string relationship;
-
-            //if (result < 0)
-            //    relationship = "is earlier than";
-            //else if (result == 0)
-            //    relationship = "is the same time as";
-            //else
-            //    relationship = "is later than";
-
-            //Console.WriteLine("{0} {1} {2}", ini, relationship, fin);
-            Console.ReadKey();
-        }*/
-
         static double GetWorkingHours(DateTime config_start_time, DateTime config_end_time, DateTime worked_start_time, DateTime worked_end_time) {
 
             var result = DateTime.Compare(worked_start_time, config_start_time);
@@ -50,30 +21,6 @@ namespace PaymentCalculator {
 
             return fin.Subtract(ini).TotalHours;
         }
-
-        /*static double GetWorkingHours( DateTime config_start_time, DateTime config_end_time, DateTime worked_start_time, DateTime worked_end_time) {
-            
-            var result = DateTime.Compare(worked_start_time, config_start_time);
-
-            if (result >= 0) {
-                // worked_start_time Es posterior o igual a config_start_time
-
-                var result2 = DateTime.Compare(worked_end_time, config_end_time);
-                if(result2 < 0) {
-                    // worked_end_time Es anterior a config_end_time
-                    return worked_end_time.Subtract(worked_start_time).TotalHours;
-                } else {
-                    // worked_end_time Es posterior o igual a config_end_time
-                    return config_end_time.Subtract(worked_start_time).TotalHours;
-                }
-            } else {
-                // worked_start_time Es anterior a config_start_time
-                // Trabaja con config_start_time
-            }
-
-            return 0;
-        }*/
-
 
         static void Main(string[] args) {
 
@@ -186,32 +133,6 @@ namespace PaymentCalculator {
                 }
 
             }
-
-            //double moneyToPay = 0;
-            //var start = new DateTime(2022, 02, 12, 8, 0, 0); //8   15    10
-            //var end = new DateTime(2022, 02, 12, 13, 0, 0); // 13   19    12
-            //var configToApply = paymentConfig.Where(c => c.Day.Equals("MO")
-            //                    && ((c.StartTime <= start && c.EndTime >= start)
-            //                    || (c.StartTime <= end && c.EndTime >= end))
-            //                    ).ToList();
-
-            //double totalHours = 0;
-
-            //foreach (var rangeConfigPayment in configToApply) {
-            //    var salary = (double)rangeConfigPayment.Salary;
-            //    var hours = GetWorkingHours(rangeConfigPayment.StartTime, rangeConfigPayment.EndTime, start, end);
-            //    Console.WriteLine($"Config   >>>> {rangeConfigPayment.StartTime.ToShortTimeString()} - {rangeConfigPayment.EndTime.ToShortTimeString()}");
-            //    Console.WriteLine($"Workging >>>> {start.ToShortTimeString()} - {end.ToShortTimeString()}");
-            //    Console.WriteLine($"Hours    >>>> {Math.Ceiling(hours)}");
-            //    Console.WriteLine($"Paying   >>>> {Math.Ceiling(hours) * salary}");
-
-            //    totalHours += Math.Ceiling(hours);
-            //    moneyToPay += (Math.Ceiling(hours) * salary);
-            //}
-            //Console.WriteLine("--------------------------------------------------------");
-            //Console.WriteLine($"Total Hours    >>>> {totalHours}");
-            //Console.WriteLine($"Total Paying   >>>> {moneyToPay}");
-
 
             Console.ReadKey();
         }
