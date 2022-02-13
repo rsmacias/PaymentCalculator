caae09f12f3c6d57c4e34b56ef30b01b27b0c467
Robert Macias <rsmacias@gmail.com>
2022-02-13 18:11:19 -0500

    Se remueve método GetWorkingHours del método principal ya que no es usado en este contexto.

--- PaymentCalculator/Program.cs
+++ PaymentCalculator/Program.cs
@@ -51,21 +51,6 @@ namespace PaymentCalculator {
             Console.ReadKey();
         }
 
-        static double GetWorkingHours(DateTime config_start_time, DateTime config_end_time, DateTime worked_start_time, DateTime worked_end_time) {
-
-            var result = DateTime.Compare(worked_start_time, config_start_time);
-            var ini = result >= 0 ? worked_start_time : config_start_time;
-
-            var result2 = DateTime.Compare(worked_end_time, config_end_time);
-            var fin = result2 < 0 ? worked_end_time : config_end_time;
-
-            var hours = fin.Subtract(ini).TotalHours;
-            if (hours < 0)
-                fin = fin.AddDays(1);
-
-            return fin.Subtract(ini).TotalHours;
-        }
-
         /// <summary>
         /// Cargar la configuración de los pagos por las horas trabajadas
         /// </summary>
