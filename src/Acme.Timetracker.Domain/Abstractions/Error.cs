af4f8397896fb13f4befe513e5ad44e8453a4635
Robert Macías <Robert.Macias@trialcard.com>
2024-03-19 20:38:23 -0500

    feat: added support to return a "Result" type with an "Error" type.

---
+++ src/Acme.Timetracker.Domain/Abstractions/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace Acme.Timetracker.Domain.Abstractions;
+
+public record Error(string Code, string Message) 
+{
+    public static Error None = new(string.Empty, string.Empty);
+
+    public static Error NullValue = new("Error.NullValue", "Null value was provided");
+}
