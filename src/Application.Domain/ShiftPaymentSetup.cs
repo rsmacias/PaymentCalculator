d596a81aaf722b5454392978386baa44f2f46120
Robert Macías <rsmacias@gmail.com>
2025-06-15 14:07:16 -0500

    ref(domain): added the entity for the shift payment configuration.

---
+++ src/Application.Domain/ShiftPaymentSetup.cs
@@ -0,0 +1,38 @@
+﻿namespace Application.Domain;
+
+public enum DayTypes
+{
+    WorkingDay = 1,
+    Holiday = 2,
+    PaidTimeOff = 3,
+    OutOfOffice = 4,
+    SickLeave = 5
+}
+
+public sealed class ShiftPaymentSetup
+{
+    public long Id { get; private set; }
+    public DayOfWeek Day { get; private set; }
+    public DayTypes Type { get; private set; }
+    public TimeOnly StartHour { get; private set; }
+    public TimeOnly EndHour { get; private set; }
+    public double Payment { get; private set; }
+    public bool IsActive { get; private set; }
+
+    public ShiftPaymentSetup(
+        long id, 
+        DayOfWeek day, 
+        DayTypes type, 
+        TimeOnly startHour, 
+        TimeOnly endHour, 
+        double payment)
+    {
+        Id = id;
+        Day = day;
+        Type = type;
+        StartHour = startHour;
+        EndHour = endHour;
+        Payment = payment;
+        IsActive = true;
+    }
+}
